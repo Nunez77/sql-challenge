f772d8b1aa7a2aa77e58c7ed829132637b48011e
Nunez77 <79233160+Nunez77@users.noreply.github.com>
2021-05-22 19:20:22 -0500

    Corrections to Table Creation Script

--- QuickDBD-Pewlett Hackard.sql
+++ QuickDBD-Pewlett Hackard.sql
@@ -1,70 +1,37 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/IaAaU9
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
-CREATE TABLE "Titles" (
-    "title_id" string   NOT NULL,
-    "title" string   NOT NULL
+﻿CREATE TABLE "Titles" (
+    "title_id" VARCHAR(10)   NOT NULL,
+    "title" VARCHAR(30)   NOT NULL
 );
 
 CREATE TABLE "Salaries" (
     "emp_no" int   NOT NULL,
     "salary" int   NOT NULL,
-    CONSTRAINT "pk_Salaries" PRIMARY KEY (
-        "emp_no"
-     )
 );
 
 CREATE TABLE "Employees" (
     "emp_no" int   NOT NULL,
-    "emp_title_id" string   NOT NULL,
+    "emp_title_id" VARCHAR(10)   NOT NULL,
     "birth_date" date   NOT NULL,
-    "first_name" string   NOT NULL,
-    "last_name" string   NOT NULL,
-    "sex" string   NOT NULL,
+    "first_name" VARCHAR(30)   NOT NULL,
+    "last_name" VARCHAR(30)   NOT NULL,
+    "sex" VARCHAR(10)   NOT NULL,
     "hire_date" date   NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
-        "emp_no"
      )
 );
 
 CREATE TABLE "Dept_Manager" (
-    "dept_no" string   NOT NULL,
+    "dept_no" VARCHAR(10)   NOT NULL,
     "emp_no" int   NOT NULL,
-    CONSTRAINT "pk_Dept_Manager" PRIMARY KEY (
-        "emp_no"
      )
 );
 
 CREATE TABLE "Dept_Emp" (
-    "emp_mo" int   NOT NULL,
-    "dept_no" string   NOT NULL,
-    CONSTRAINT "pk_Dept_Emp" PRIMARY KEY (
-        "emp_mo"
+    "emp_no" int   NOT NULL,
+    "dept_no" VARCHAR(10)   NOT NULL,
      )
 );
 
 CREATE TABLE "Deparments" (
-    "dept_no" string   NOT NULL,
-    "dept_name" string   NOT NULL
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "dept_name" VARCHAR(10)   NOT NULL
 );
-
-ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_title_id" FOREIGN KEY("title_id")
-REFERENCES "Employees" ("emp_title_id");
-
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
-
-ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Salaries" ("emp_no");
-
-ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_emp_mo" FOREIGN KEY("emp_mo")
-REFERENCES "Employees" ("emp_no");
-
-ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Dept_Manager" ("dept_no");
-
-ALTER TABLE "Deparments" ADD CONSTRAINT "fk_Deparments_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Dept_Emp" ("dept_no");
-
