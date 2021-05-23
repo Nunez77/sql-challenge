0217ab97874757e367732367f2e9ac9c5ef81fb2
Nunez77 <79233160+Nunez77@users.noreply.github.com>
2021-05-22 19:22:36 -0500

    Tables Created After Syntax Corrections

--- QuickDBD-Pewlett Hackard.sql
+++ QuickDBD-Pewlett Hackard.sql
@@ -5,7 +5,7 @@
 
 CREATE TABLE "Salaries" (
     "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL,
+    "salary" int   NOT NULL
 );
 
 CREATE TABLE "Employees" (
@@ -15,20 +15,17 @@ CREATE TABLE "Employees" (
     "first_name" VARCHAR(30)   NOT NULL,
     "last_name" VARCHAR(30)   NOT NULL,
     "sex" VARCHAR(10)   NOT NULL,
-    "hire_date" date   NOT NULL,
-     )
+    "hire_date" date   NOT NULL
 );
 
 CREATE TABLE "Dept_Manager" (
     "dept_no" VARCHAR(10)   NOT NULL,
-    "emp_no" int   NOT NULL,
-     )
+    "emp_no" int   NOT NULL
 );
 
 CREATE TABLE "Dept_Emp" (
     "emp_no" int   NOT NULL,
-    "dept_no" VARCHAR(10)   NOT NULL,
-     )
+    "dept_no" VARCHAR(10)   NOT NULL
 );
 
 CREATE TABLE "Deparments" (
